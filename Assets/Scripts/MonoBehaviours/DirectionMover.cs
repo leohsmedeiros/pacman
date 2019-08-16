01a5f5065c12307550bf525de3d5854ac282b389
Leonardo Medeiros <>
2019-08-16 07:45:05 -0300

    - fixed the animation transition

---
+++ Assets/Scripts/MonoBehaviours/DirectionMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DirectionMover : MonoBehaviour {
+    public enum Direction { RIGHT, LEFT, UP, DOWN };
+
+    public float speed;
+
+    private Vector3 directionVector { get; set; } = Vector3.zero;
+
+    public void ChangeDirection(Direction direction) {
+        switch (direction) {
+            case Direction.RIGHT:
+                directionVector = Vector3.right;
+                break;
+
+            case Direction.LEFT:
+                directionVector = Vector3.left;
+                break;
+
+            case Direction.UP:
+                directionVector = Vector3.up;
+                break;
+
+            case Direction.DOWN:
+                directionVector = Vector3.down;
+                break;
+        }
+    }
+
+    private void Update() {
+        this.transform.position += (directionVector * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/Pacman.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(DirectionMover))]
+public class Pacman : MonoBehaviour {
+    private Animator animator;
+    private DirectionMover move;
+
+    private void Start() {
+        animator = this.GetComponent<Animator>();
+        move = this.GetComponent<DirectionMover>();
+        animator.SetTrigger("right");
+    }
+
+    //
+    void Update() {
+        float HorizontalAxis = Input.GetAxisRaw("Horizontal");
+        float VerticalAxis = Input.GetAxisRaw("Vertical");
+
+        if (HorizontalAxis > 0) {
+            animator.SetTrigger("right");
+            move.ChangeDirection(DirectionMover.Direction.RIGHT);
+        } else if (HorizontalAxis < 0) {
+            animator.SetTrigger("left");
+            move.ChangeDirection(DirectionMover.Direction.LEFT);
+        } else if (VerticalAxis > 0) {
+            animator.SetTrigger("up");
+            move.ChangeDirection(DirectionMover.Direction.UP);
+        } else if (VerticalAxis < 0) {
+            animator.SetTrigger("down");
+            move.ChangeDirection(DirectionMover.Direction.DOWN);
+        }
+    }
+
+    //public void ChangeDirection (PacmanDirection direction) {
+    //    switch(direction) {
+    //        case PacmanDirection.RIGHT:
+    //            this.transform.eulerAngles = new Vector3(0, 0, 0);
+    //            move.direction = Vector3.right;
+    //            break;
+
+    //        case PacmanDirection.LEFT:
+    //            this.transform.eulerAngles = new Vector3(0, 0, 180);
+    //            move.direction = Vector3.left;
+    //            break;
+
+    //        case PacmanDirection.UP:
+    //            this.transform.eulerAngles = new Vector3(0, 0, 90);
+    //            move.direction = Vector3.up;
+    //            break;
+
+    //        case PacmanDirection.DOWN:
+    //            this.transform.eulerAngles = new Vector3(0, 0, 270);
+    //            move.direction = Vector3.down;
+    //            break;
+    //    }
+    //}
+}
