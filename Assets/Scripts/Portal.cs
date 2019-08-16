18c3a162bac792abbf7c2528bf813ccac6f9db64
Leonardo Medeiros <>
2019-08-16 00:53:14 -0300

    - feito o portal

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Portal : MonoBehaviour {
+
+    public Vector3 futurePoint;
+    public Pacman.PacmanDirection direction;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag.Equals("Player")) {
+            collision.transform.position = futurePoint;
+            collision.gameObject.GetComponent<Pacman>().ChangeDirection(direction);
+        }
+    }
+}
