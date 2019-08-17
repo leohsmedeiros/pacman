08f8ee1e9c931a1ea98cd36ec0215a31778d2dae
Leonardo Medeiros <>
2019-08-17 13:33:41 -0300

    - feitas as animações do fantasma vermelho e criado um script para ele

--- Assets/Scripts/MonoBehaviours/UiController.cs
+++ Assets/Scripts/MonoBehaviours/UiController.cs
@@ -1,25 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UiController : MonoBehaviour /*, IObserverProperty<int>*/ {
 
+    public GameObject Ready;
     public Text TextScore;
     public Text TextHighScore;
 
-    // Start is called before the first frame update
+
+    IEnumerator HideReadyWithSeconds(int seconds) {
+        yield return new WaitForSeconds(seconds);
+        Ready.SetActive(false);
+    }
+
     void Start() {
         TextHighScore.text = PlayerPrefs.GetInt("highscore", 0).ToString();
         TextScore.text = "0";
+
+        StartCoroutine(HideReadyWithSeconds(5));
 
         GameController controller = GameController.GetInstance();
 
         //((IReactiveProperty<int>)controller).Subscribe(this);
     }
 
-
     public void OnUpdateProperty(int value) {
         TextScore.text = value.ToString();    
     }
-
 
 }
