e3258e48d387a549668a78d306b52bf62845fe09
Leonardo Medeiros <>
2019-08-18 19:30:29 -0300

    - feita a IA do Clyde

---
+++ Assets/Scripts/MonoBehaviours/Ghosts/ClydeGhostAi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClydeGhostAi : GhostAi {
+    protected override Vector2 EstimateTargetPoint() {
+        Vector2 pacmanPosition = _pacman.transform.position;
+        float distance = Vector2.Distance(_currentNode.GetPosition2D(), pacmanPosition);
+
+        if (distance > 8) {
+            return _pacman.transform.position;
+        }else {
+            return scatterModeTarget.GetPosition2D();
+        }
+    }
+}
